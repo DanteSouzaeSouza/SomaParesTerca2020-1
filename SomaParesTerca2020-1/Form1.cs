00ce056f098176a05ccffdfd6291686360e8096a
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-12 21:53:28 -0300

    Final commit

--- SomaParesTerca2020-1/Form1.cs
+++ SomaParesTerca2020-1/Form1.cs
@@ -21,25 +21,57 @@ namespace SomaParesTerca2020_1
         private void btnCalcular_Click(object sender, EventArgs e)
         {
             // Criando as variáveis que receberão o número digitado e a soma
-            int num, soma = 0;
+            int num = 0, multiplo = 0, soma = 0;
 
             // Captando o número digitado e jogando na variável num
-            num = Convert.ToInt32(txtNum.Text);
 
-            // Criando a estrutura de repetição do código que fará a soma
-            for (int i = 1; i <= num; i++)
+            // checando se os valores digitados são realmente números inteiros
+            bool numValido;
+            bool multiploValido;
+            numValido = int.TryParse(txtNum.Text, out num);
+            multiploValido = int.TryParse(txtMultiplo.Text, out multiplo);
+
+
+            // se num for diferente de 0 e múltiplo igual a 0,
+            // Calcula a soma dos pares
+            if (num != 0 && multiplo == 0)
             {
-                // checando se i é par
-                if (i % 2 == 0)
+                // Criando a estrutura de repetição do código que fará a soma
+                for (int i = 1; i <= num; i++)
                 {
-                    // somando o que está na variável soma com o valor de i
-                    soma = soma + i; // pode ser escrito como soma += i
-                    // Checar os passos da soma
-                    Console.WriteLine("Número: "+ i + ". Soma: " + soma);
+                    // checando se i é par
+                    if (i % 2 == 0)
+                    {
+                        // somando o que está na variável soma com o valor de i
+                        soma = soma + i; // pode ser escrito como soma += i
+                        // Checar os passos da soma
+                        Console.WriteLine("Número: " + i + ". Soma: " + soma);
+                    }
                 }
+
+                // jogando o total da soma no txtSoma
+                txtSoma.Text = soma.ToString();
+            }// se num for diferente de 0 e múltiplo também,
+            // Calcula a soma dos múltiplos
+            else if (num != 0 && multiplo != 0)
+            {
+                // Criando a estrutura de repetição do código que fará a soma
+                for (int i = 1; i <= num; i++)
+                {
+                    // checando se i é múltiplo do número informado
+                    if (i % multiplo == 0)
+                    {
+                        // somando o que está na variável soma com o valor de i
+                        soma = soma + i; // pode ser escrito como soma += i
+                        // Checar os passos da soma
+                        Console.WriteLine("Número: " + i + ". Soma: " + soma);
+                    }
+                }
+
+                // jogando o total da soma no txtSoma
+                txtSoma.Text = soma.ToString();
+
             }
-            // jogando o total da soma no txtSoma
-            txtSoma.Text = soma.ToString();
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
@@ -47,6 +79,7 @@ namespace SomaParesTerca2020_1
             // Limpando os controles para novo cálculo
             txtSoma.Clear();
             txtNum.Clear();
+            txtMultiplo.Clear();
 
         }
     }
